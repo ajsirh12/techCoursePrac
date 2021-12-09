a31e2d1f1f3cbee3df23defccfb62580fb973975
ajsirh12 <ajsirh31@gamil.com>
2021-12-09 15:00:43 +0900

    refactor : 인터페이스 상수 제거

--- racingGame/src/racingGame/exception/CarInputException.java
+++ racingGame/src/racingGame/exception/CarInputException.java
@@ -1,19 +1,20 @@
 package racingGame.exception;
 
-import racingGame.view.Constant;
+import static racingGame.constant.CarExceptionConstant.*;
 
-public class CarInputException implements Constant {
-	
-	private static final String SPLIT_WORD = ",";
+public class CarInputException {
 	
 	public void carException(String cars) {
-		
 		String[] carArr = cars.split(SPLIT_WORD);
 		
 		for(String str : carArr) {
-			if(str.length() <= 0 || str.length() > 5) {
-				throw new IllegalArgumentException(INCORRECT_CAR_NAME);
-			}
+			chkLength(str);
+		}
+	}
+	
+	private void chkLength(String str) {
+		if(str.length() <= 0 || str.length() > 5) {
+			throw new IllegalArgumentException(INCORRECT_CAR_NAME);
 		}
 	}
 	
