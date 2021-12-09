45bea1224687636a26378322017055dda5f46e20
ajsirh12 <ajsirh31@gamil.com>
2021-12-09 15:46:56 +0900

    feat : CarInputView

---
+++ racingGame/src/racingGame/view/CarInputView.java
@@ -0,0 +1,10 @@
+package racingGame.view;
+
+import static racingGame.constant.ViewConstant.INPUT_CAR_NAME;
+
+public class CarInputView {
+	
+	public void carInput() {
+		System.out.println(INPUT_CAR_NAME);
+	}
+}
