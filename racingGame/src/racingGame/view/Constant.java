1979386d951e7bd4c32780f1647a9f5f2d1dd18e
ajsirh12 <ajsirh31@gamil.com>
2021-12-09 14:04:21 +0900

    feat : Constant 인터페이스 추가

---
+++ racingGame/src/racingGame/view/Constant.java
@@ -0,0 +1,13 @@
+package racingGame.view;
+
+public interface Constant {
+	
+	public static final String INCORRECT_CAR_NAME = "[ERROR] : 이름 입력이 잘못되었습니다.";
+	
+	public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분";
+	public static final String INPUT_TRY_CNT = "시도할 횟수는 몇 회인가요?";
+	
+	public static final String PROC_RESULT = "실행 결과";
+	public static final String FINAL_RESULT = "최종 우승자 : ";
+
+}
