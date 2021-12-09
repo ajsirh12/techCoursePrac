02a3cffab10dbbae91d587cc1c913454783c5257
ajsirh12 <ajsirh31@gamil.com>
2021-12-09 15:01:27 +0900

    feat : 상수 클래스 추가

---
+++ racingGame/src/racingGame/constant/CarExceptionConstant.java
@@ -0,0 +1,11 @@
+package racingGame.constant;
+
+public class CarExceptionConstant {
+	
+	private CarExceptionConstant() {
+		
+	}
+	
+	public static final String INCORRECT_CAR_NAME = "[ERROR] : 이름 입력이 잘못되었습니다.";
+	public static final String SPLIT_WORD = ",";
+}
