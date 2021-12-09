01052052b9626e6e27fd6889425c6dc534aa19ac
ajsirh12 <ajsirh31@gamil.com>
2021-12-09 16:02:05 +0900

    feat : resultView 추가

--- racingGame/src/racingGame/view/ResultView.java
+++ racingGame/src/racingGame/view/ResultView.java
@@ -1,5 +1,9 @@
 package racingGame.view;
 
+import static racingGame.constant.ViewConstant.FINAL_RESULT;
+
+import java.util.List;
+
 import racingGame.domain.Car;
 
 public class ResultView {
@@ -14,6 +18,21 @@ public class ResultView {
 		for(int i=0; i<num; i++) {
 			result += "_";
 		}
+		
+		return result;
+	}
+	
+	public void resultView(List<Car> carList) {
+		System.out.println(FINAL_RESULT + lastWinner(carList));
+	}
+	
+	private String lastWinner(List<Car> carList) {
+		String result = "";
+		
+		for(Car car : carList) {
+			result += (car.getName() + ", ");
+		}
+		result = result.substring(0, result.length() - 2);
 		
 		return result;
 	}
