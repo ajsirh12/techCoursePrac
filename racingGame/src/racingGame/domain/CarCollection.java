22154fd11c609f655eb0fe1df6ad2dad4cdcf82a
ajsirh12 <ajsirh31@gamil.com>
2021-12-09 17:45:44 +0900

    feat : CarCollection 생성

---
+++ racingGame/src/racingGame/domain/CarCollection.java
@@ -0,0 +1,30 @@
+package racingGame.domain;
+
+import java.util.List;
+
+public class CarCollection {
+	
+	List<Car> cars;
+	
+	public CarCollection(List<Car> cars) {
+		this.cars = cars;
+	}
+	
+	public String getCarName(int i) {
+		Car car = cars.get(i);
+		
+		return car.getName();
+	}
+	
+	public int getCarPosition(int i) {
+		Car car = cars.get(i);
+		
+		return car.getPosition();
+	}
+	
+	public void moveCarPosition(int i) {
+		Car car = cars.get(i);
+		
+		car.carMove();
+	}
+}
