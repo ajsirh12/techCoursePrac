8d1ec483709aac9e953085c45f69ac41f16d708e
ajsirh12 <ajsirh31@gamil.com>
2021-12-09 15:34:19 +0900

    feat : 시도횟수 입력 Exception 상수 추가

---
+++ racingGame/src/racingGame/constant/InputExceptionConstant.java
@@ -0,0 +1,10 @@
+package racingGame.constant;
+
+public class InputExceptionConstant {
+	
+	private InputExceptionConstant() {
+		
+	}
+	
+	public static final String INCORRECT_NUM = "[ERROR] : 숫자가 아닙니다.";
+}
