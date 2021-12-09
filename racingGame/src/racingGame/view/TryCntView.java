b69c1921065bcdac81c7051522b4526f758338ea
ajsirh12 <ajsirh31@gamil.com>
2021-12-09 15:47:31 +0900

    feat : TryCntView

---
+++ racingGame/src/racingGame/view/TryCntView.java
@@ -0,0 +1,10 @@
+package racingGame.view;
+
+import static racingGame.constant.ViewConstant.INPUT_TRY_CNT;
+
+public class TryCntView {
+
+	public void cntView() {
+		System.out.println(INPUT_TRY_CNT);
+	}
+}
